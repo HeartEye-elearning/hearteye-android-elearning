41fcabce02989c391502c03c7d76d9cec4be1c2c
jessekops <645147@student.inholland.nl>
2024-11-15 15:28:11 +0100

    Course overview card

--- app/src/main/java/nl/hearteye/elearning/ui/components/coursecard/CourseCard.kt
+++ app/src/main/java/nl/hearteye/elearning/ui/components/coursecard/CourseCard.kt
@@ -1,4 +1,109 @@
 package nl.hearteye.elearning.ui.components.coursecard
 
-class CourseCard {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import nl.hearteye.elearning.R
+import nl.hearteye.elearning.ui.theme.BackgroundBlue
+import nl.hearteye.elearning.ui.theme.ForegroundBlue
+import nl.hearteye.elearning.ui.theme.typography
+
+@Composable
+fun CourseCard(
+    title: String,
+    time: String
+) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .width(300.dp),
+        shape = androidx.compose.foundation.shape.RoundedCornerShape(CornerSize(10.dp)),
+        colors = androidx.compose.material3.CardDefaults.cardColors(containerColor = Color.White),
+        elevation = androidx.compose.material3.CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Box(
+                modifier = Modifier
+                    .shadow(
+                        elevation = 4.dp,
+                        shape = androidx.compose.foundation.shape.RoundedCornerShape(8.dp),
+                        clip = false
+                    )
+                    .clip(androidx.compose.foundation.shape.RoundedCornerShape(8.dp))
+                    .background(Color.White)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.quiz_cover),
+                    contentDescription = "Course Cover",
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(133.dp)
+                )
+            }
+
+            Row {
+                Text(
+                    text = title,
+                    style = typography.bodyLarge,
+                    color = Color.Black
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .background(
+                            color = BackgroundBlue,
+                            shape = androidx.compose.foundation.shape.RoundedCornerShape(6.dp)
+                        )
+                        .padding(horizontal = 4.dp, vertical = 4.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_clock),
+                            contentDescription = "Time Icon",
+                            modifier = Modifier
+                                .width(14.dp)
+                                .height(14.dp),
+                            tint = ForegroundBlue
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = "${time}min",
+                            style = typography.bodySmall,
+                            color = ForegroundBlue
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/nl/hearteye/elearning/ui/theme/Color.kt
+++ app/src/main/java/nl/hearteye/elearning/ui/theme/Color.kt
@@ -4,3 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val ForegroundPrimary = Color(0xFFFF5D01)
 val BackgroundPrimary = Color(0xFFF9FBFF)
+val BackgroundBlue = Color(0xFFEAF4FF)
+val ForegroundBlue = Color(0xFF3D8FEF)
