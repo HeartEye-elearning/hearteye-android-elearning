78fc0dad89269c6a1380aeb30aa293c5fe1cb5eb
jessekops <645147@student.inholland.nl>
2024-11-18 15:28:22 +0100

    Search bar

---
+++ app/src/main/java/nl/hearteye/elearning/ui/components/searchbar/SearchBar.kt
@@ -0,0 +1,73 @@
+package nl.hearteye.elearning.ui.components.searchbar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    value: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .shadow(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(8.dp),
+                clip = false
+            )
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.White)
+            .height(40.dp),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                contentDescription = "Search Icon",
+                modifier = Modifier.padding(start = 8.dp)
+            )
+            Spacer(modifier = Modifier.width(2.dp))
+            TextField(
+                value = value,
+                onValueChange = onValueChange,
+                placeholder = {
+                    Text(text = "Search Courses...")
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                singleLine = true,
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchBarPreview() {
+    SearchBar(value = TextFieldValue(""), onValueChange = {})
+}
--- app/src/main/java/nl/hearteye/elearning/ui/screens/courses/CoursesViewModel.kt
+++ app/src/main/java/nl/hearteye/elearning/ui/screens/courses/CoursesViewModel.kt
@@ -18,10 +18,25 @@ class CoursesViewModel @Inject constructor(
     private val _courses = mutableStateOf<List<Course>>(emptyList())
     val courses: State<List<Course>> = _courses
 
+    private val _filteredCourses = mutableStateOf<List<Course>>(emptyList())
+    val filteredCourses: State<List<Course>> = _filteredCourses
+
     fun getCourses(language: String = "eng") {
         viewModelScope.launch {
             val fetchedCourses = courseRepository.getCourses(language)
             _courses.value = fetchedCourses
+            _filteredCourses.value = fetchedCourses
+        }
+    }
+
+    fun filterCourses(query: String) {
+        _filteredCourses.value = if (query.isEmpty()) {
+            _courses.value
+        } else {
+            _courses.value.filter { course ->
+                course.title.contains(query, ignoreCase = true)
+            }
         }
     }
 }
+
