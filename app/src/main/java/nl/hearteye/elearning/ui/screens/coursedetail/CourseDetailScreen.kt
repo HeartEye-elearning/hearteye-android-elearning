1cc910c6491ac24023eaf06b314eda75dc2a3122
jessekops <645147@student.inholland.nl>
2024-11-21 17:12:55 +0100

    Styling progress

--- app/src/main/java/nl/hearteye/elearning/ui/screens/coursedetail/CourseDetailScreen.kt
+++ app/src/main/java/nl/hearteye/elearning/ui/screens/coursedetail/CourseDetailScreen.kt
@@ -1,6 +1,9 @@
 package nl.hearteye.elearning.ui.screens.coursedetail
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -10,6 +13,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import nl.hearteye.elearning.ui.theme.ForegroundPrimary
@@ -33,8 +38,10 @@ fun CourseDetailScreen(
     Box(modifier = Modifier.fillMaxSize()) {
         when {
             isLoading -> {
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center),
-                    color = ForegroundPrimary)
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center),
+                    color = ForegroundPrimary
+                )
             }
             errorMessage != null -> {
                 Text(
@@ -47,27 +54,39 @@ fun CourseDetailScreen(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(16.dp)
+                        .background(
+                            Color.White,
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .shadow(5.dp, shape = RoundedCornerShape(10.dp))
+                        .padding(16.dp)
                 ) {
                     if (!isStarted.value) {
                         Text(
                             text = courseDetail.title,
                             modifier = Modifier.align(Alignment.CenterHorizontally)
-                                .padding(bottom = 32.dp)
+                                .padding(bottom = 32.dp),
+                            style = androidx.compose.material3.MaterialTheme.typography.titleMedium
                         )
                         Button(
                             onClick = {
                                 isStarted.value = true
                                 currentPageIndex.value = 0
                             },
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(bottom = 16.dp),
+                            shape = RoundedCornerShape(10.dp),
+                            colors = androidx.compose.material3.ButtonDefaults.buttonColors(containerColor = ForegroundPrimary)
                         ) {
-                            Text(text = "Start")
+                            Text(text = "Start", color = Color.White)
                         }
                     } else {
                         val currentInformationPage = courseDetail.informationPages.getOrNull(currentPageIndex.value)
                         if (currentInformationPage != null) {
                             Text(
-                                text = currentInformationPage.content["eng"] ?: "No content available"
+                                text = currentInformationPage.content["eng"] ?: "No content available",
+                                modifier = Modifier.padding(bottom = 16.dp)
                             )
                         }
 
@@ -79,17 +98,27 @@ fun CourseDetailScreen(
                         ) {
                             if (currentPageIndex.value > 0) {
                                 Button(
-                                    onClick = { currentPageIndex.value-- }
+                                    onClick = { currentPageIndex.value-- },
+                                    modifier = Modifier.weight(1f),
+                                    shape = RoundedCornerShape(10.dp),
+                                    border = BorderStroke(1.dp, Color.Red),
+                                    colors = androidx.compose.material3.ButtonDefaults.outlinedButtonColors(contentColor = ForegroundPrimary),
                                 ) {
-                                    Text(text = "Back")
+                                    Text(
+                                        text = "Back",
+                                        color = ForegroundPrimary
+                                    )
                                 }
                             }
 
                             if (currentPageIndex.value < courseDetail.informationPages.size - 1) {
                                 Button(
-                                    onClick = { currentPageIndex.value++ }
+                                    onClick = { currentPageIndex.value++ },
+                                    modifier = Modifier.weight(1f),
+                                    shape = RoundedCornerShape(10.dp),
+                                    colors = androidx.compose.material3.ButtonDefaults.buttonColors(containerColor = ForegroundPrimary)
                                 ) {
-                                    Text(text = "Next")
+                                    Text(text = "Next", color = Color.White)
                                 }
                             } else {
                                 Text(
@@ -103,4 +132,5 @@ fun CourseDetailScreen(
         }
     }
 }
+
 
