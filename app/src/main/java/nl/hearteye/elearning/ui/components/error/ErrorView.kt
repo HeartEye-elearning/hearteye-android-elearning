b8bd2993f5ea984d0563a9c631b3d64fc679adc8
jessekops <645147@student.inholland.nl>
2024-11-21 15:49:11 +0100

    loading and error state

---
+++ app/src/main/java/nl/hearteye/elearning/ui/components/error/ErrorView.kt
@@ -0,0 +1,37 @@
+package nl.hearteye.elearning.ui.components.error
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ErrorView(message: String, onRetry: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            color = Color.Red,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = onRetry) {
+            Text("Retry")
+        }
+    }
+}
--- app/src/main/java/nl/hearteye/elearning/ui/screens/courses/CoursesScreen.kt
+++ app/src/main/java/nl/hearteye/elearning/ui/screens/courses/CoursesScreen.kt
@@ -3,22 +3,30 @@ package nl.hearteye.elearning.ui.screens.courses
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import nl.hearteye.elearning.ui.components.coursecard.CourseCard
+import nl.hearteye.elearning.ui.components.error.ErrorView
 import nl.hearteye.elearning.ui.components.searchbar.SearchBar
+import nl.hearteye.elearning.ui.theme.ForegroundPrimary
 
 @Composable
 fun CoursesScreen(
     courseViewModel: CoursesViewModel = hiltViewModel()
 ) {
     val courses = courseViewModel.filteredCourses.value
+    val isLoading = courseViewModel.isLoading.value
+    val errorMessage = courseViewModel.errorMessage.value
     val searchQuery = remember { mutableStateOf("") }
 
     LaunchedEffect(Unit) {
@@ -29,28 +37,56 @@ fun CoursesScreen(
         courseViewModel.filterCourses(searchQuery.value)
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        SearchBar(
-            value = TextFieldValue(searchQuery.value),
-            onValueChange = { searchQuery.value = it.text },
-            modifier = Modifier.padding(20.dp)
-        )
-
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 5.dp)
-        ) {
-            items(courses.size) { index ->
-                val course = courses[index]
-                CourseCard(
-                    title = course.title,
-                    time = "${course.duration}"
+    Box(modifier = Modifier.fillMaxSize()) {
+        when {
+            isLoading -> {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center),
+                    color = ForegroundPrimary
                 )
+            }
+            errorMessage != null -> {
+                ErrorView (
+                    message = errorMessage,
+                    onRetry = { courseViewModel.getCourses(language = "eng") }
+                )
+            }
+            courses.isEmpty() -> {
+                Text(
+                    text = "No courses available",
+                    modifier = Modifier.align(Alignment.Center),
+                    color = Color.Gray
+                )
+            }
+            else -> {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    SearchBar(
+                        value = TextFieldValue(searchQuery.value),
+                        onValueChange = { searchQuery.value = it.text },
+                        modifier = Modifier.padding(20.dp)
+                    )
+
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 5.dp)
+                    ) {
+                        items(courses.size) { index ->
+                            val course = courses[index]
+                            CourseCard(
+                                title = course.title,
+                                time = "${course.duration}"
+                            )
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+
+
 
 
